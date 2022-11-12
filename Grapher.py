e651fee2bd0a45734620c072df11fd3e762ea8e2
Samuel Lennard <sl2g19@soton.ac.uk>
2022-11-12 18:03:17 +0000

    grapher now produces graph and raw metric data

--- Grapher.py
+++ Grapher.py
@@ -1,6 +1,10 @@
 from pathlib import Path
 import pandas as pd
-from .metrics.BaseMetric import BaseMetric
+from metrics.BaseMetric import BaseMetric
+from metrics.CentreDistMetric import CentreDistMetric
+from metrics.Separation import Separation
+
+from matplotlib import pyplot as plt
 
 
 class Grapher():
@@ -53,7 +57,7 @@ class Grapher():
         """
         ind_var_output = {}
         for ind_var in directory.iterdir():
-            df = self.get_single_val_data(metric, directory)
+            df = self.get_single_val_data(metric, ind_var)
             result = None
             if reduction == "mean":
                 result = pd.concat([df["Timestep"], df.loc[:, df.columns != "Timestep"].mean(axis=1)], axis=1)
@@ -67,8 +71,8 @@ class Grapher():
         
         return ind_var_output
 
-    def get_all_var_data(self, metric_list: dict[str: BaseMetric], directory: Path, graph_func=None,
-                         save_data: bool=True) -> dict[str, dict[str, pd.DataFrame]]:
+    def get_all_var_data(self, metric_list: dict[str: BaseMetric], directory: Path, reduction="mean", graph_func=None,
+                         save_data: bool=True, *graph_args) -> dict[str, dict[str, pd.DataFrame]]:
         """
         Takes in a list of metrics and a directory of multilpe variables changing and produces the metric data 
         of the simulations in that directory. There will be data for a set of graphs for each variable for each metric.
@@ -83,12 +87,38 @@ class Grapher():
             A dictionary linking each variable name to a metric dictionary, which links each metric name to a data dictionary,
             which links each variable value to the values of the metric at each timestep (a bit disgusting i know).
         """
-        pass
+        data_folder = Path("out/Metric_Data")
+        fig_folder = Path("out/Graphs")
+        if save_data:
+            data_folder.mkdir(parents=True, exist_ok=True)
+        if graph_func:
+            fig_folder.mkdir(parents=True, exist_ok=True)
+        
+        for var in directory.iterdir():
+            for metric_name, metric in metric_list.items():
+                name = "{} - {}".format(var.name, metric_name)
+                print(name)
+                ind_var_output = self.get_single_var_data(metric, var, reduction=reduction)
+                if save_data:
+                    folder = data_folder / var.name / metric_name    
+                    for k, v in ind_var_output.items():
+                        f = folder / k
+                        f.mkdir(parents=True, exist_ok=True)
+                        v.to_csv(path_or_buf=(f / "metric_data.csv"), index=False)
 
-    def generate_line_charts(self, data_list):
-        pass
+                if graph_func is not None:
+                    fig = graph_func(ind_var_output)
+                    folder = fig_folder / var.name / metric_name
+                    folder.mkdir(parents=True, exist_ok=True)
+                    fig.savefig(folder / (metric_name + ".png"))
 
-    def generate_bar_charts(self, data_list):
+    def generate_bar_chart(self, data):
+        fig = plt.figure()
+        for k,d in data.items():
+            plt.plot(d["Timestep"].to_numpy(), d.loc[:, d.columns != "Timestep"].to_numpy(), label=k, figure=fig)
+        return fig
+
+    def generate_line_chart(self, data_list):
         pass
 
     def save_data(self, data_list):
@@ -97,9 +127,10 @@ class Grapher():
     def save_graphs(self, figs):
         pass
 
-    
-
 
 
 if __name__ == "__main__":
-    pass
+    grapher = Grapher()
+    metric_list = {"cdm": CentreDistMetric(), "sep_min": Separation(), "sep_max": Separation(reduction="max")}
+    p = Path("out/FOLLOW_CIRCLE")
+    grapher.get_all_var_data(metric_list, p, graph_func=grapher.generate_bar_chart)
