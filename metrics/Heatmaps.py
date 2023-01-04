e347d0172e7b00f1b05e85d15071933ebf78a6cf
ykaf1g18 <ykaf1g18@soton.ac.uk>
2023-01-04 23:49:06 +0000

    updated heatmap

--- metrics/Heatmaps.py
+++ metrics/Heatmaps.py
@@ -74,9 +74,59 @@ def GetHeatmap(mission,para,run,bins,path):
     fig.savefig((para + run +".png"), bbox_inches="tight")
 
 
+def GetSingleHeatmap(mission,para,value,run,bins,path):
+    pathname = "../out/"+mission+'/'+para+'/'+value+'/'+run+'/'+"/raw_data_log.csv"
+
+    data0 = pd.read_csv(pathname)
+    x_p0 = data0["X Position"]
+    y_p0 = data0["Y Position"]
+    x_array0 = x_p0.to_numpy()
+    y_array0 = y_p0.to_numpy()
+
+    heatmap0, xedges0, yedges0 = np.histogram2d(x_array0, y_array0, bins=bins)
+    minp = min(xedges0[0],xedges0[-1],yedges0[0],yedges0[-1])
+    maxp = max(xedges0[0],xedges0[-1],yedges0[0],yedges0[-1])
+    extent0 = [minp,maxp ,minp , maxp]
+    x_points = np.linspace(start = minp, stop = maxp, num = 3)
+
+    plt.imshow(heatmap0.T, cmap = 'turbo', extent=extent0, origin='lower')
+    #plt.xaxis.set_major_locator(FixedLocator([round(item, -2) for item in x_points]))
+
+    if path and mission == 'FOLLOW_CIRCLE_ULTRA_EXTENDED_DATA':
+        plt.add_patch(Circle((2500,2500),1000, fill = False, color = '#39FF14'))
+    elif path and mission == 'RACETRACK_EXTENDED':
+        r = 1000
+        lw = 1
+
+        arc_angles = linspace(pi/2, 2*pi*(3/4), 30)
+        arc_xs = r * cos(arc_angles) + 1500
+        arc_ys = r * sin(arc_angles) + 2500
+
+        arc_angles2 = linspace(-1*pi*(1/2),pi/2, 30)
+        arc_xs2 = r * cos(arc_angles2) + 3500
+        arc_ys2 = r * sin(arc_angles2) + 2500
+
+        p1 = [1500,1500]
+        p2 = [3500,1500]
+        x, y = [p1[0], p2[0]], [p1[1], p2[1]]
+
+        p3 = [1500,3500]
+        p4 = [3500,3500]
+        x2, y2 = [p3[0], p4[0]], [p3[1], p4[1]]
+
+        plt.plot(arc_xs, arc_ys, color = '#39FF14', lw = lw)
+        plt.plot(arc_xs2, arc_ys2, color = '#39FF14', lw = lw)
+        plt.plot( x , y , lw=lw , color = '#39FF14')
+        plt.plot( x2 , y2 , lw=lw ,color = '#39FF14' )
+
+        plt.show()
+
+
+
+
 if __name__ == "__main__":
 
-    bins = 200
+    bins = 250
     run = '4'
     mission = {
         '1' : 'FOLLOW_CIRCLE_ULTRA_EXTENDED_DATA',
@@ -100,16 +150,8 @@ if __name__ == "__main__":
         '13' : 'SPEED_ERROR',
         }
 
-    for x in range(5):
+    GetSingleHeatmap(mission['3'],parameters['3'],'1.0','0',bins,1)
 
-        GetHeatmap(mission['1'],parameters['2'],str(x),bins,1)
-        GetHeatmap(mission['1'],parameters['3'],str(x),bins,1)
-        GetHeatmap(mission['1'],parameters['5'],str(x),bins,1)
-        GetHeatmap(mission['1'],parameters['6'],str(x),bins,1)
-        GetHeatmap(mission['1'],parameters['8'],str(x),bins,1)
-        GetHeatmap(mission['1'],parameters['9'],str(x),bins,1)
-        GetHeatmap(mission['1'],parameters['11'],str(x),bins,1)
-        GetHeatmap(mission['1'],parameters['13'],str(x),bins,1)
 
 
     print("done")
