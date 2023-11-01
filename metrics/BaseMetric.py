101ba83831d39a726c72d1fa4603f5fabc56000d
sharminakthar <sharmin060401@hotmail.co.uk>
2023-11-01 22:13:24 +0000

    Added base class for metrics

---
+++ metrics/BaseMetric.py
@@ -0,0 +1,33 @@
+import pandas as pd
+from abc import ABC, abstractmethod
+import os
+
+class BaseMetric(ABC):
+    """
+    Abstract class defining key methods for every metric
+    """
+
+    def __init__(self):
+        pass
+
+    def read_csv(self, csv_file: str) -> pd.DataFrame:
+        pass
+
+    def run_metric(self, folder):
+        for run in os.listdir(folder):
+            data = pd.read_csv(run)
+            
+    
+    @abstractmethod
+    def calculate(self, data: pd.DataFrame) -> pd.DataFrame:
+        """
+        Runs the metric on the raw data of a single simulation
+
+        Parameters:
+            data: pd.Dataframe
+                The raw data to run the metric on
+        
+        Returns:
+            A pandas dataframe with one column for the timestep and another column for the metric.
+        """
+        pass
