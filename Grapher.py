fb633af3a31794b6bd08bd3b5ea2abb461cfc6b0
sharminakthar <sharmin060401@hotmail.co.uk>
2023-11-01 22:16:45 +0000

    Skeleton code for grapher

---
+++ Grapher.py
@@ -0,0 +1,26 @@
+from pathlib import Path
+
+class Grapher():
+    
+    def __init__(self):
+        pass
+
+    def get_single_var_data(self, metric_list, directory: Path):
+        pass
+
+    def get_all_var_data(self, metric_list, directory: Path):
+        pass
+
+    def generate_line_charts(self, data_list):
+        pass
+
+    def generate_bar_charts(self, data_list):
+        pass
+
+    def save_graphs(self, figs):
+        pass
+
+
+
+if __name__ == "__main__":
+    pass
