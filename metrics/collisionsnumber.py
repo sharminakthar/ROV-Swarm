80e06b6c1d1cc73b320f1162b5c5f387c744e0fb
sharminakthar <sharmin060401@hotmail.co.uk>
2023-11-01 22:16:15 +0000

    updated collisionsnumber with efficient code

--- metrics/collisionsnumber.py
+++ metrics/collisionsnumber.py
@@ -12,42 +12,38 @@ class CollisionsNumber(BaseMetric):
     def __init__(self):
         super().__init__()
 
-    def myfunction(self, data: pd.DataFrame) -> pd.DataFrame:
-        df = data.merge(data, how="cross")
-        df = df.loc[df["Drone ID_x"] < df["Drone ID_y"]]
-
-        distances = np.sqrt( ((df["X Position_x"] - df["X Position_y"]).pow(2)) + ((df["Y Position_x"] - df["Y Position_y"]).pow(2)))
-
-        df["Distances"] = distances
-
-        df.loc[df["Distances"] < 75, 'Less_than_separation_distance'] = True
-        df.loc[df["Distances"] >= 75, 'Less_than_separation_distance'] = False
-
-        collisions = df.Less_than_separation_distance[df.Less_than_separation_distance==True].count()
-        return(collisions)
+    def myfunction(self, df: pd.DataFrame, index1, index2) -> pd.DataFrame:
+        arr1 = df.to_numpy()[index1]
+        arr2 = df.to_numpy()[index2]
+        distances = np.linalg.norm(arr1[:,1:] - arr2[:,1:], axis=1)
+        return (distances < 7.5).sum()
 
     def calculate(self, data: pd.DataFrame) -> pd.DataFrame:
-        df = data[["Timestep" ,"Drone ID", "X Position", "Y Position"]]     
-        print("grouping and applying")
+        df = data[["Timestep", "X Position", "Y Position"]]     
+        
+        i = len(df.loc[df["Timestep"] == 0])
+        ia2, ib2 = np.broadcast_arrays(*np.ogrid[:i,:i])
+        indices = np.triu_indices(i,1)
+        index1 = ia2[indices]
+        index2 = ib2[indices]
 
-        df= df.groupby('Timestep').apply(self.myfunction).reset_index()
-        df = df.rename(columns={0: "Collisions"})
+        df1= df.groupby('Timestep').apply(self.myfunction, index1=index1, index2=index2).reset_index()
 
-        print(df)
-        return(df)
+        return df1
 
 if __name__ == "__main__":
     metric = CollisionsNumber()
 
     # Replace path name with absolute path if not running from inside the metrics folder
-    path_name = "/Users/sharmin/Desktop/GDP/swarm-simulator/out/FLOCK_SIZE"
+    path_name = "../out/FLOCK_SIZE"
     p = Path(path_name)
     print("running")
     data = metric.run_metric(p)
-    # "list comprehension"
+    # "dictionary comprehension"
     heights = {k:0 for k in data.keys()}
     for k,d in data.items():
-        val = d.loc[:, d.columns != "Timestep"].to_numpy().sum() /( d.shape[1] -1 )
+        # Get mean number of collisions over each run
+        val = d.loc[:, d.columns != "Timestep"].to_numpy().sum() / ( d.shape[1] - 1 )
         heights[k] = val
     plt.bar(heights.keys(), heights.values())
     plt.legend()
