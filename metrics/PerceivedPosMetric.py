c2f845c6aed1963152526773a3a74edfca983849
sharminakthar <sharmin060401@hotmail.co.uk>
2023-11-01 22:15:53 +0000

    Perceived position metric completed

---
+++ metrics/PerceivedPosMetric.py
@@ -0,0 +1,33 @@
+from pathlib import Path
+from BaseMetric import BaseMetric
+import pandas as pd
+import numpy as np
+from matplotlib import pyplot as plt
+
+class PerceivedPosMetric(BaseMetric):
+
+    def __init__(self):
+        super().__init__()
+    
+    def calc_error(self, df: pd.DataFrame) -> pd.DataFrame:
+        x_error = df["X Position"] - df["X Approx Position"]
+        y_error = df["Y Position"] - df["Y Approx Position"]
+        error = np.sqrt(x_error.pow(2) + y_error.pow(2))
+        return error.mean()
+    
+    def calculate(self, data: pd.DataFrame) -> pd.DataFrame:
+        df = data[["Timestep", "X Position", "Y Position", "X Approx Position", "Y Approx Position"]]
+        return df.groupby("Timestep").apply(self.calc_error).reset_index()
+
+if __name__ == "__main__":
+    metric = PerceivedPosMetric()
+
+    # Replace path name with absolute path if not running from inside the metrics folder
+    path_name = "../out/FLOCK_SIZE"
+    p = Path(path_name)
+
+    data = metric.run_metric(p)
+    for k,d in data.items():
+        plt.plot(d["Timestep"], d.loc[:, d.columns != "Timestep"].mean(axis=1), label=k)
+    plt.legend()
+    plt.show()
